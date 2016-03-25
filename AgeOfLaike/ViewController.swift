cf12c75cbaab9c38e571803e3074740f070549c7
Hector Rios <oj.rios@gmail.com>
2016-03-25 23:26:59 +0100

    finished app

--- AgeOfLaike/ViewController.swift
+++ AgeOfLaike/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var imageViewField: UIImageView!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let humanYearsAsInt = humanYearsTextField.text.toInt()!
+        var dogYears = humanYearsAsInt * 7;
+        dogYearsLabel.text = "Dog Years are: \(dogYears)"
+        
+        humanYearsTextField.resignFirstResponder()
+    }
 
 
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        let multiplier = 10.5
+        let humanYearsAsString = humanYearsTextField.text
+        let humanYearsAsDouble = Double((humanYearsAsString as NSString).doubleValue)
+        
+        var actualAge:Double
+        
+        if (humanYearsAsDouble > 2) {
+            //Do logic here
+            actualAge = (multiplier * 2) + (4 * (humanYearsAsDouble - 2))
+        } else {
+            actualAge = multiplier * humanYearsAsDouble
+        }
+        
+        //update the label
+        dogYearsLabel.text = "Dog Years are: \(actualAge)"
+        
+        humanYearsTextField.resignFirstResponder()
+
+    }
 }
 
